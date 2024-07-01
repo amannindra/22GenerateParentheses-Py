f9351ca1cc6452efcb5b3464f13d248c8203da5e
Aman Nindra <amannindra@gmail.com>
2024-06-30 21:06:36 -0700

    one problem done

---
+++ Python/66.py
@@ -0,0 +1,17 @@
+class Solution:
+    def plusOne(self, digits):
+        
+        
+        output = ""
+        for i in range(len(digits)):
+            output += str(digits[i])
+        output = int(output)
+        output += 1
+        output = str(output)
+        s = []
+        for i in output:
+            f = int(i) 
+            s.append(f)
+        return s
+        # s += 1
+        # return list(s)
